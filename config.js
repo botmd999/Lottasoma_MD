d8ec6d7f62e6287b4ea2928c190c1cb3f1e787c7
botmd999 <150532064+botmd999@users.noreply.github.com>
2023-11-11 20:20:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,26 +6,26 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
 global.port= process.env.PORT || 5000
-global.email = 'sam@sec.live'
-global.github = 'https://github.com/prabathLK/PRABATH-MD'
-global.location = 'Sultanpur IN'
+global.email = 'botmd999@gmail.com'
+global.github = 'https://github.com/botmd999/Lottasoma-MD'
+global.location = 'SriLank LK'
 global.gurl = 'https://instagram.com/' // add your username
 global.devs = '94762280384';
-global.website = 'https://github.com/prabathLK/PRABATH-MD' //wa.me/+94762280384
+global.website = 'https://github.com/botmd999/Lottasoma-MD' //wa.me/+94762280384
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/H062oEI.jpeg'
 module.exports = {
   OPENAI_API_KEY:process.env.OPENAI_API_KEY || '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',
-  botname: process.env.BOT_NAME || 'PRABATH-MD',
+  botname: process.env.BOT_NAME || '𝙻𝙾𝚃𝚃𝙰𝚂𝙾𝙼𝙰 ᵏᵘᵐᵃʳᵃʸᵃ',
   dl_limit: process.env.DL_SIZE || '300',
   art: process.env.AUTO_RESTART || 'true',
   sudo: process.env.SUDO || '94762280384',
   alivepic: process.env.ALIVE_IMG || 'https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg',
-  alivemsj: process.env.ALIVE_MSJ || '*POWERD BY PRABATH-MD* 🎊',
-  ownername:process.env.OWNER_NAME || 'PRABATH',
+  alivemsj: process.env.ALIVE_MSJ || '*POWERD BY Lottasoma* 🎊',
+  ownername:process.env.OWNER_NAME || '𝙻𝙾𝚃𝚃𝙰𝚂𝙾𝙼𝙰 ᵏᵘᵐᵃʳᵃʸᵃ',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
-  auto_read_status : process.env.AUTO_READ_STATUS || 'false',
-  packname: process.env.PACK_INFO.split(";")[1] || 'Name',
+  auto_read_status : process.env.AUTO_READ_STATUS || 'true',
+  packname: process.env.PACK_INFO.split(";")[1] || '𝙻𝙾𝚃𝚃𝙰𝚂𝙾𝙼𝙰 ᵏᵘᵐᵃʳᵃʸᵃ',
   autoreaction: process.env.AUTO_REACTION || 'off',
   antibadword : process.env.ANTI_BAD_WORD || 'fuck,huth,fucking,huththa,huththo,hukanna,fucked,pakaya,pako,ponnaya,ponnayo,kariya,kariyo,huththaa,hutta',
   alwaysonline: process.env.ALWAYS_ONLINE || 'true',
@@ -34,8 +34,8 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ['.'],
   warncount : process.env.WARN_COUNT || 3,
   disablepm: process.env.DISABLE_PM || "flase",
-  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false',
-  antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
+  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'truee',
+  antilink: process.env.ANTILINK_VALUES || 'tsapp.com',
   antilinkaction: process.env.ANTILINK_ACTION || 'remove',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || 'put your herokuapp name',
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || 'put your heroku_api_key',
